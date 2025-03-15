0b54cc9299b4a4309d98d12bf944892fb34c02fa
congo musah adama <musahcongoadama@gmail.com>
2025-03-15 19:01:38 +0000

    swagger-doc

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import connectDB from './src/config/db.js';
-import userRoutes from './src/routes/userRoutes.js';
+import authRoutes from './src/routes/authRoutes.js';
+import setupSwagger from './src/utils/swagger.js'; // Import Swagger setup
+
 
 const app = express();
 
@@ -14,11 +16,15 @@ app.use(express.json());
 
 
 
-// Define routes here (to be added later)
-app.use('/api/users', userRoutes);
+setupSwagger(app); // Setup Swagger documentation
+
+app.use('/api/users', authRoutes);
+
+
 
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port http://localhost:${PORT}`);
+    console.log(`Swagger documentation available at http://localhost:${PORT}/api-docs`); // Log Swagger URL
 });
