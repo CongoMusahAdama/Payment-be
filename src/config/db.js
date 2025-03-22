9427af227b8f01533548df5cce176842349ceb84
congo musah adama <musahcongoadama@gmail.com>
2025-03-22 16:35:47 +0000

    Fixed MongoDB connection by removing deprecated options

--- src/config/db.js
+++ src/config/db.js
@@ -5,15 +5,10 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.DB_URI, {
-
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
+    const conn = await mongoose.connect(process.env.DB_URI); 
     console.log(`MongoDB Connected successfully: ${conn.connection.host}`);
   } catch (error) {
-    console.error(`Error: ${error.message}`);
+    console.error(` MongoDB Connection Error: ${error.message}`);
     process.exit(1); // Exit process on failure
   }
 };
