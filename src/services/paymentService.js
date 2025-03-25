4d8277a505f5cc7aa074f2c7b952342179a0ac30
congo musah adama <musahcongoadama@gmail.com>
2025-03-25 23:00:48 +0000

    withdrawal bug fixes

--- src/services/paymentService.js
+++ src/services/paymentService.js
@@ -155,7 +155,16 @@ export const handleWebhookUpdated = async (req, res) => {
  */
 export const initiateWithdrawal = async (user, recipientCode, amount, otp) => {
   try {
-    const response = await axios.post("https://api.paystack.co/transfer", {
+    console.log("📤 Withdrawal request payload:", {
+      recipient: recipientCode,
+      amount: amount * 100, // Paystack expects the amount in kobo
+      currency: "NGN",
+      reason: "Withdrawal request",
+      otp: otp, // Include OTP if required
+    });
+
+    const response = await axios.post("https://api.paystack.co/transfer", { 
+
       recipient: recipientCode,
       amount: amount * 100, // Paystack expects the amount in kobo
       currency: "NGN",
