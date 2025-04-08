0c6b529040c7db205f39b30f6ae2371a045d884d
congo musah adama <musahcongoadama@gmail.com>
2025-04-08 11:53:26 +0000

    server could not be reach

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.use('/api/reports', reportRoutes);
 
 // Start the server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server running on port http://localhost:${PORT}`);
+app.listen(PORT,'0.0.0.0', () => {
+    console.log(`Server running on port http://0.0.0.0:${PORT}`);
     console.log(`Swagger documentation available at http://localhost:${PORT}/api-docs`);
 });
